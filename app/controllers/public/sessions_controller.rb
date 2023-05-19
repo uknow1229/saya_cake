7d67c92cca5b3c2f5c4db8b711165b1f1516edd5
saya <uknow.love1229@gmail.com>
2023-05-20 00:10:28 +0900

    [Add]customer-login

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,4 +1,20 @@
-class Public::SessionsController < ApplicationController
+class Public::SessionsController < Devise::SessionsController
+  before_action :configure_sign_in_params,  if: :devise_controller?
   def new
+  end
+  
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+  protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:first_name, :last_name, :furigana_first_name, :furigana_last_name, :post_code, :address, :telephone_number])
   end
 end
